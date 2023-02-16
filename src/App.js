9b04d2480e9c83d9293331c68254769265506b4b
HwangMinHyeok <aldur930@gmail.com>
2023-02-16 17:03:27 +0900

    Anya

--- src/App.js
+++ src/App.js
@@ -6,17 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="https://www.vogue.co.uk/fashion/article/anya-taylor-joy-british-vogue-interview">Vogue: Anya Taylor-Joy</a>
       </header>
     </div>
   );
