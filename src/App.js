29e33efc3335d0d2d8169c70531be6b1e75aa07e
HwangMinHyeok <aldur930@gmail.com>
2023-02-16 17:14:48 +0900

    branch practice

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <a href="https://www.youtube.com/watch?v=m3sM0zIG1jY">OMG by New Jeans</a>
+        <div>new branch practice</div>
       </header>
     </div>
   );
